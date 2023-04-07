72e53cde744fecd798a49e880e86b4fd12bb3aa8
bloodnighttw <bbeenn1227@gmail.com>
2023-04-07 18:28:26 +0800

    make better

---
+++ js/blocker.js
@@ -0,0 +1,7 @@
+﻿$(function(){
+    if($(".adbox").height() > 1 && $(".adbox").css("display") != "none"){
+        console.log("no ad blocker");
+    }else{
+        $("#body").replaceWith('<div class="container" id="body"><h1>請關閉你的廣告阻擋器</h1></div>')
+    }
+})
